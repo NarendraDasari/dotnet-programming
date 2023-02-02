8981e3208775254d6ce40906bd37423c28818743
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-02-02 13:34:25 -0600

    using ifelse condition.

--- Com.Chaitanya/Com.Chaitanya.Learning.StartUp/Program.cs
+++ Com.Chaitanya/Com.Chaitanya.Learning.StartUp/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Com.Chaitanya.Learning.StartUp
+﻿
+
+using Com.Chaitanya.Programming.Concepts.Conditional.Loops;
+
+namespace Com.Chaitanya.Learning.StartUp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello, World!");
+
+
+
+
+            // this two lines are to test IfElse condition.
+            IfElseCondition con=new IfElseCondition();
+            
+            con.TestIfelseCondition();
+
+
+            //This lines are for testing while loop.
+                
         }
     }
 }
--- Com.Chaitanya/Com.Chaitanya.Programming.Concepts/Conditional.Loops/IfElseCondition.cs
+++ Com.Chaitanya/Com.Chaitanya.Programming.Concepts/Conditional.Loops/IfElseCondition.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Com.Chaitanya.Programming.Concepts.Conditional.Loops
 {
-    internal class IfElseCondition
+    public class IfElseCondition
     {
+
+        public void TestIfelseCondition()
+        {
+            Console.WriteLine("Executing IfElse Condition.");
+            Console.WriteLine("Enter ur firstname:");
+            
+
+
+           string firstname= Console.ReadLine();
+
+            Console.WriteLine("Enter lastname");
+
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Enter Middlename");
+            string middlename = Console.ReadLine();
+
+            if(lastname=="")
+            {
+                greetUser(firstname);
+            }
+            else if(middlename=="")
+            {
+                greetUser(firstname, lastname);
+            }
+            else
+            {
+                greetUser(firstname, middlename, lastname);
+            }
+            
+
+        }
+        private void greetUser(string firstname)
+        {
+            Console.WriteLine("Welcome ," + firstname);
+            
+        }
+
+        private void greetUser(string firstname,string lastname)
+        {
+            Console.WriteLine("Welcome ," + firstname +" " + lastname);
+        }
+
+        private void greetUser(string firstname,string middlename,string lastname)
+        {
+            Console.WriteLine("Welcome ," + firstname + " " + middlename + " " + lastname);
+        }
+
+
     }
 }
