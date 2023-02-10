6092c3629be4e0b57f9cd08c8ecc9e4ba1cca727
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-02-09 22:34:55 -0600

    Different datatypes of objects added to arraylist.while the retrieving the databases we did explicit typecasting.

--- Com.Chaitanya/Com.Chaitanya.Programming.Concepts/Collections/CollectionsDemo.cs
+++ Com.Chaitanya/Com.Chaitanya.Programming.Concepts/Collections/CollectionsDemo.cs
@@ -5,8 +5,10 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Com.Chaitanya.Models;
 
 namespace Com.Chaitanya.Programming.Concepts.Collections
+    
 {
     public class CollectionsDemo
     {
@@ -14,31 +16,62 @@ namespace Com.Chaitanya.Programming.Concepts.Collections
         {
             //Console.WriteLine("Testing Collections Method.");
             ArrayList list = new ArrayList();
-            //list.Add("chaitanya");
-            //list.Add("narendra");
-            //list.Add("dasari");
-            //list.Add("31");
+
+            // system automatically does boxing to convert value types to object types while assigning into ArrayList
+            list.Add("chaitanya");
+            list.Add("narendra");
+            list.Add("dasari");
+            list.Add(31);
 
 
-            string choice = "N";
-            do
-            {
-                Console.WriteLine("Enter ur item:");
-                string item = Console.ReadLine();
-                list.Add(item);
-                Console.WriteLine("Do u want to continue");
-                choice = Console.ReadLine();
 
 
-            } while (choice == "Y" || choice == "y");
+            Person per = new Person();
+            per.FirstName = "aaaaaa";
+            per.LastName = "bbbbbb";
+            per.DateOfBirth = "03/2/2000";
+            list.Add(per);
 
 
+            Console.WriteLine("String name of Person class:" + per.ToString());
+
+
+            //string choice = "N";
+            //do
+            //{
+            //    Console.WriteLine("Enter ur item:");
+            //    string item = Console.ReadLine();
+            //    list.Add(item);
+            //    Console.WriteLine("Do u want to continue");
+            //    choice = Console.ReadLine();
+
+
+            //} while (choice == "Y" || choice == "y");
+
+
+            
+            
 
 
                 Console.WriteLine("number of items:" + list.Count);
             for(int i=0,x=1;i<list.Count;i++,x++)
             {
-                Console.WriteLine( x + ". value in the list:" +list[i]);
+                
+                if(list[i].ToString() == "Com.Chaitanya.Models.Person")
+                {
+                    // explicit type casting is required here, as system cannot implicitly covert object  to Person type.
+                    Person obj = (Person)list[i];
+                    Console.WriteLine(x + ". value in the list:" + obj.FirstName);
+                    Console.WriteLine(x + ".value in the list:" + obj.LastName);
+                    Console.WriteLine(x + ".value in the list:" + obj.DateOfBirth);
+
+                }
+                else
+                {
+                    // system automatically un-box the object types to value types
+                    Console.WriteLine(x + ". value in the list:" + list[i]);
+                }
+                
 
             }
 
