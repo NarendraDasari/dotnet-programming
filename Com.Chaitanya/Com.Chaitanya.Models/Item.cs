c2de29dd04dcd09b3c9597b7fa5eb793c217591e
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-02-12 12:21:12 -0600

    Added New Item Model Class

---
+++ Com.Chaitanya/Com.Chaitanya.Models/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace Com.Chaitanya.Models
+{
+    public class Item
+    {
+
+        /// <summary>
+        /// Gets or Sets Id of the Item
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the Text/Name of the Item
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
